fdca4fbe265fa38022e69dde68479c2ec886c375
MMA <25148407+MMA92@users.noreply.github.com>
2025-11-07 12:12:27 +0100

    another fix for a bug

--- Snake Multiplayer/snake.py
+++ Snake Multiplayer/snake.py
@@ -26,7 +26,11 @@ powerups = PowerUp(particle_size=particle)
 
 
 def resource_path(rel_path: str) -> str:
-    base_path = getattr(sys, "_MEIPASS", os.path.dirname(os.path.abspath(__file__)))
+    try:
+        base_path = sys._MEIPASS
+        base_path = os.path.join(base_path, "Snake Multiplayer")
+    except Exception:
+        base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, rel_path)
 
 
