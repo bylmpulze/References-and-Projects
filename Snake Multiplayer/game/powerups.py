b23213a7f9242eecca4b823345678f7c56c8dc83
MMA <25148407+MMA92@users.noreply.github.com>
2025-11-07 12:06:18 +0100

    fixed for new folder structure

--- Snake Multiplayer/game/powerups.py
+++ Snake Multiplayer/game/powerups.py
@@ -8,7 +8,7 @@ import sys
 def resource_path(rel_path: str) -> str:
     try:
         base_path = sys._MEIPASS
-        base_path = os.path.join(base_path, "game")
+        base_path = os.path.join(base_path, "Snake Multiplayer/game")
     except Exception:
         base_path = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(base_path, rel_path)
