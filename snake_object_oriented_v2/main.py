11b0e267c16ca35604569f9bddc047510f742f32
bylmpulze <steffen.maurer1993@gmail.com>
2025-11-12 22:12:37 +0100

    addet file: main.py
    addet file: main.py

---
+++ snake_object_oriented_v2/main.py
@@ -0,0 +1,22 @@
+import pygame
+from game.game_render import GameScreen
+
+pygame.init()  
+
+my_display = GameScreen()
+clock = pygame.time.Clock()
+running = True
+
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    my_display.show_gameWindow.fill(my_display.background_colour)
+    my_display.draw_topbar()
+    pygame.display.update()
+    clock.tick(60)
+
+pygame.quit()
+
