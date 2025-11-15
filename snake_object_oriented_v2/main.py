26083fffe468928b7a4009dbb290a0cb0d9139bd
bylmpulze <steffen.maurer1993@gmail.com>
2025-11-15 23:53:02 +0100

    added background + Snake printing into the mainloop
    added background + Snake printing into the mainloop

--- snake_object_oriented_v2/main.py
+++ snake_object_oriented_v2/main.py
@@ -1,21 +1,37 @@
 import pygame
-from game_lib.game_render import GameScreen
+from game_lib.game_render import GameScreen as GameScreen
+from game_lib.snake import SnakeDisplay
+
 
 pygame.init()  
 
-my_display = GameScreen()
+game_render = GameScreen()
 clock = pygame.time.Clock()
-running = True
+game_Started = True
+endgame = True
+snakedisplay = SnakeDisplay(game_render)
+snakedisplay.get_SnakeImages()
+
+
 
 #region Mainloop
-while running:
+while game_Started:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            game_Started = False
+        if event.type == pygame.KEYDOWN:
+            snakedisplay.snake_direction = snakedisplay.handle_normal_movement(event,snakedisplay.snake_direction)
 
-    my_display.show_gameWindow.fill(my_display.background_colour)
-    my_display.draw_topbar()
+
+    #draw background
+    game_render.show_gameWindow.fill(game_render.background_colour)
+    #draw snake
+    game_render.draw_topbar(),snakedisplay.draw_snake(),snakedisplay.snake_movement(), snakedisplay.wrap_around()
+
+    
+    
     pygame.display.update()
+    snakedisplay.move_counter += 1
     clock.tick(60)
 
 pygame.quit()
