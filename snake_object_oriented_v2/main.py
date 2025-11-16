04f81415e69ba3ed6c7cfa4804002e9d11b9c9b5
bylmpulze <steffen.maurer1993@gmail.com>
2025-11-16 03:12:10 +0100

    added food into game loop

--- snake_object_oriented_v2/main.py
+++ snake_object_oriented_v2/main.py
@@ -1,16 +1,22 @@
 import pygame
-from game_lib.game_render import GameScreen as GameScreen
+from game_lib.game_render import GameScreen  #importiert die klasse Gamescreen von der datei game_render in dem ordner game_lib
 from game_lib.snake import SnakeDisplay
+from game_lib.food import FoodMain
 
 
 pygame.init()  
 
-game_render = GameScreen()
+game_render = GameScreen() #erstellt ein objekt "game render" von der klasse "GameScreen"
+snake_Display = SnakeDisplay(game_render) #erstellt ein objekt "snakedisplay" von der Klasse snakedisplay und gibt das objekt game_render mit - Das bedeutet SnakeDisplay hat zugriff auf die klasse GameScreen() 
+food_Display = FoodMain(game_render)
+
+
+#init pictures - snake/food
+snake_Display.create_snake_body_image(),snake_Display.create_snake_head_image(),food_Display.create_foodImage()
 clock = pygame.time.Clock()
 game_Started = True
 endgame = True
-snakedisplay = SnakeDisplay(game_render)
-snakedisplay.get_SnakeImages()
+food_Display.spawn_food(snake_Display.get_snake_cords()) #init foodspawn
 
 
 
@@ -20,18 +26,25 @@ while game_Started:
         if event.type == pygame.QUIT:
             game_Started = False
         if event.type == pygame.KEYDOWN:
-            snakedisplay.snake_direction = snakedisplay.handle_normal_movement(event,snakedisplay.snake_direction)
+            snake_Display.snake_direction = snake_Display.handle_normal_movement(event,snake_Display.snake_direction)
 
 
     #draw background
     game_render.show_gameWindow.fill(game_render.background_colour)
-    #draw snake
-    game_render.draw_topbar(),snakedisplay.draw_snake(),snakedisplay.snake_movement(), snakedisplay.wrap_around()
+    game_render.draw_topbar()
+    
+    #draw snake/food
+    snake_Display.draw_snake(),food_Display.draw_food()
+    
+    #snake Movement
+    snake_Display.snake_movement(), snake_Display.wrap_around()
+
+
 
     
     
     pygame.display.update()
-    snakedisplay.move_counter += 1
+    snake_Display.move_counter += 1
     clock.tick(60)
 
 pygame.quit()
