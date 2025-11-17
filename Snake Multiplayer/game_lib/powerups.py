3dbc71710ceb16bef8cc8f59afa74a2aaf217131
bylmpulze <steffen.maurer1993@gmail.com>
2025-11-18 00:16:30 +0100

    added powerup_jump class

--- Snake Multiplayer/game_lib/powerups.py
+++ Snake Multiplayer/game_lib/powerups.py
@@ -9,6 +9,7 @@ class PowerUpConfig:
         self.powerup_immunity = False  
         self.powerup_magnet = False
         self.powerup_change_direction = False
+        self.powerup_jump = False
 
         self.powerup_change_direction_duration = 1000 #Dauer - umgekehrte steuerung  (1000 = 1 sec) 
         self.powerup_magnet_duration = 1000 #Dauer Apfel heranziehen  (1000 = 1 sec) 
@@ -31,53 +32,6 @@ class PowerUpMain:
         self.spawn_duration = 1000 # spawnzeit nach letztem Powerup ( 1000 = 1 sec) 
         self.config = PowerUpConfig ()
         self.menu = PowerupSettingsMenu() 
-        self.powerupTypes = {
-            "powerup_speed_boost_x2": {
-                "menu_att": self.menu,
-                "menu_button": "settingsMenu_speedx2",
-                "duration_menu": "settingsMenu_speedx2_duration",
-                "config_": self.config,
-                "config_activ": "powerup_change_direction",
-                "config_duration": "speed_boost_x2_duration",
-                "image": "assets/powerup_speed2.png"
-            },
-            "powerup_speed_half": {
-                "menu_att": self.menu,
-                "menu_button": "settingsMenu_speedHalf",
-                "duration_menu": "settingsMenu_speedHalf_duration",
-                "config_att": self.config,
-                "config_duration": "powerup_speed_half_duration",
-                "image": "assets/powerup_speedhalf.png"
-            },
-            "powerup_magnet": {
-                "menu_att": self.menu,
-                "menu_button": "settingsMenu_powerup_magnet",
-                "duration_menu": "settingsMenu_magnet_duration",
-                "config_att": self.config,
-                "config_activ": "powerup_change_direction",
-                "config_duration": "powerup_magnet_duration",
-                "image": "assets/powerup_magnet.webp"
-            },
-            "powerup_change_direction": {
-                "menu_att": self.menu,
-                "menu_button": "settingsMenu_change_direction",
-                "duration_menu": "settingsMenu_change_direction_duration",
-                "config_att": self.config,
-                "config_activ": "powerup_change_direction",
-                "config_duration": "powerup_change_direction_duration",
-                "image": "assets/powerup_drunk.jpg"
-            },
-            "powerup_immunity": {
-                "menu_att": self.menu,
-                "menu_button": "settingsMenu_powerup_immunity",
-                "duration_menu": "settingsMenu_powerup_immunity",
-                "config_att": self.config,
-                "config_activ": "powerup_immunity",
-                "config_duration": "powerup_immunity_duration",
-                "image": "assets/powerup_extra_life.png"
-            }
-        }
-
 
 #region SettingsMenu
 class PowerupSettingsMenu():
@@ -177,12 +131,18 @@ class DrunkPowerUp(PowerUp):
         img = "assets/Items/powerup drunk.png"
         super().__init__(img, x, y, 1000)
 
+class JumpPowerUp(PowerUp):
+    def __init__(self, x, y) -> None:
+        img = "assets/Items/GET_IMG_HERE!!!!.png"
+        super().__init__(img, x, y, 1000)
+
 POWERUP_CLASS_MAP = {
     "speed_boost_x2": SpeedupPowerUp,
     "speed_half": SlowDownPowerUp,
     "extra_life": ExtraLifePowerUp,
     "powerup_drunk": DrunkPowerUp,
     "powerup_magnet": MagnetPowerUp,  # once you add it
+    "powerup_jump": JumpPowerUp,
 }
 
 class PowerUps:
